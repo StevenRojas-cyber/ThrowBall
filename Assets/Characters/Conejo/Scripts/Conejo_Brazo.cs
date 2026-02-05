e7e8fae9cade62c0b3511c0334dc03fcd0c65c7a
StevenRojas-cyber <stevenisaac3103@gmail.com>
2026-02-05 18:32:34 -0300

    Creacion Brazo Conejo

---
+++ Assets/Characters/Conejo/Scripts/Conejo_Brazo.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Conejo_Brazo : MonoBehaviour
+{
+    public Sprite ArmSprite;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
